ec2b79053a992ce61dbf25682249fc0891d13cf7
Alina Shtykh <alinashtykh@gmail.com>
2024-03-06 00:12:13 +0100

    complet design

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -10,24 +10,33 @@ const TaskList = ({ tasks, onToggle }) => {
   };
   return (
     <ul className='mt-4 border-spacing-5 rounded-md flex w-full justify-between flex-col gap-5'>
-      {tasks.map((task) => (
-        <li
-          key={task.id}
-          onClick={() => onToggle(task.id)}
-          className={`bg-cyan-100 hover:bg-cyan-400 transition duration-500 ease-linear p-5 flex justify-between rounded-lg cursor-pointer text-cyan-900 ${
-            task.completed ? 'line-through' : ''
-          }`}
-        >
-          {task.name}
-
-          <button
-            className='text-red-700 text-xl  hover:scale-110 transition duration-150 ease-linear'
-            onClick={() => handleDelete(task.id)}
-          >
-            <FaTrash />
-          </button>
+      {tasks.length === 0 ? (
+        <li className='text-center text-cyan-700 text-lg mt-10'>
+          You have not added any tasks
         </li>
-      ))}
+      ) : (
+        tasks.map((task) => (
+          <li
+            key={task.id}
+            onClick={() => onToggle(task.id)}
+            className={`bg-cyan-100 hover:bg-cyan-400 transition duration-500 ease-linear p-5 flex justify-between rounded-lg cursor-pointer text-cyan-900 ${
+              task.completed ? 'line-through' : ''
+            }`}
+          >
+            {task.name}
+
+            <button
+              onClick={(e) => {
+                e.stopPropagation();
+                handleDelete(task.id);
+              }}
+              className='text-red-700 text-xl hover:scale-110 transition duration-150 ease-linear'
+            >
+              <FaTrash />
+            </button>
+          </li>
+        ))
+      )}
     </ul>
   );
 };
